dd919b92913b200c2fe39947f0c7dbbb2acb4e84
Felipe Tomelin <tomelinfelipe@live.com>
2021-09-15 00:42:30 -0300

    Estrutura Inicial

---
+++ BotDiscord/BotDiscord/Services/CommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace BotDiscord.Services
+{
+    public class CommandHandler
+    {
+        public static DiscordSocketClient _discord;
+        public static CommandService _commandService;
+        public static IConfigurationRoot _configurationRoot;
+        public static IServiceProvider _serviceProvider;
+
+        public CommandHandler(DiscordSocketClient discord, CommandService commandService,
+            IConfigurationRoot configurationRoot, IServiceProvider serviceProvider)
+        {
+            _discord = discord;
+            _commandService = commandService;
+            _configurationRoot = configurationRoot;
+            _serviceProvider = serviceProvider;
+
+            _discord.Ready += OnReady;
+        }
+
+        //Pegar nome de usuário e tag do perfil ex: Nome#0000
+        private Task OnReady()
+        {
+            Console.WriteLine($"Conectado no Bot: \nUsuário: {_discord.CurrentUser.Username}#{_discord.CurrentUser.Discriminator}");
+            return Task.CompletedTask;
+        }
+    }
+}
